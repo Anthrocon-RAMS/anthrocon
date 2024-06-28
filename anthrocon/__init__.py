e8c505788bef1f163f0c9884985172d8278fa479
Victoria Earl <kitsuta@gmail.com>
2024-06-27 21:59:50 -0400

    Finish import script

--- anthrocon/__init__.py
+++ anthrocon/__init__.py
@@ -1,12 +1,16 @@
+from datetime import datetime
+
 from os.path import join
 
 from uber.jinja import template_overrides
 from uber.utils import mount_site_sections, static_overrides
+
+from anthrocon._version import __version__  # noqa: F401
 from .config import config
-from anthrocon.automated_emails import *  # noqa: F401,E402,F403
-from anthrocon.models import *  # noqa: F401,E402,F403
 
+from . import models  # noqa: F401,E402,F403
+from . import automated_emails  # noqa: F401
 
+mount_site_sections(config['module_root'])
 static_overrides(join(config['module_root'], 'static'))
 template_overrides(join(config['module_root'], 'templates'))
-mount_site_sections(config['module_root'])
