6b3dc3423ad0a7ea8d706a69891bc7b21133ad63
Victoria Earl <kitsuta@gmail.com>
2025-02-19 18:03:30 -0500

    Remove replyto override It's not necessary, all art show emails already have this. It just doesn't work!

--- anthrocon/automated_emails.py
+++ anthrocon/automated_emails.py
@@ -13,5 +13,4 @@ ArtShowAppEmailFixture(
 
 AutomatedEmail.email_overrides.extend([
     ('art_show_confirm', 'subject', f"OFFICIAL Anthrocon Art Show reservation acknowledgement"),
-    ('art_show_confirm', 'replyto', [c.ART_SHOW_EMAIL]),
     ('art_show_confirm', 'needs_approval', False)])
