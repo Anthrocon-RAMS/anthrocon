2383df408a275e21f0df7feb9854530f3639b71b
Victoria Earl <kitsuta@gmail.com>
2025-05-15 18:20:23 -0400

    Update bidsheet base image Now we have proper margins!

--- anthrocon/models.py
+++ anthrocon/models.py
@@ -80,46 +80,46 @@ class ArtShowPiece:
         xplus = yplus = 0
 
         if sheet_num in [1, 3]:
-            xplus = 305
+            xplus = 306
         if sheet_num in [2, 3]:
-            yplus = 394
+            yplus = 396
 
         # Gallery, Piece ID and Barcode
-        pdf.image(get_static_file_path('bidsheet.png'), x=0 + xplus, y=-2 + yplus, w=305)
+        pdf.image(get_static_file_path('bidsheet.png'), x=xplus, y=yplus, w=306)
         pdf.set_font(normal_font_name, size=15)
-        pdf.set_xy(154 + xplus, 15 + yplus)
+        pdf.set_xy(154 + xplus, 27 + yplus)
         if self.gallery == c.GENERAL:
             pdf.cell(132, 22, txt="G", ln=1, align="L")
         else:
             pdf.cell(132, 22, txt="M", ln=1, align="L")
         pdf.set_font("3of9", size=27)
-        pdf.set_xy(178 + xplus, 8 + yplus)
+        pdf.set_xy(173 + xplus, 19 + yplus)
         pdf.cell(132, 22, txt=self.barcode_data, ln=1, align="C")
         pdf.set_font(bold_font_name, size=8)
-        pdf.set_xy(178 + xplus, 27 + yplus)
+        pdf.set_xy(173 + xplus, 38 + yplus)
         pdf.cell(132, 12, txt=self.artist_and_piece_id, ln=1, align="C")
 
         # Artist
         pdf.set_font(normal_font_name, size=12)
         set_fitted_font_size(self.artist_name or self.app_display_name, max_size=260)
-        pdf.set_xy(10 + xplus, 41 + yplus)
+        pdf.set_xy(12 + xplus, 51 + yplus)
         pdf.cell(230, 24, txt=(self.artist_name or self.app_display_name), ln=1, align="C")
         
         # Title
-        pdf.set_xy(10 + xplus, 65 + yplus)
         set_fitted_font_size(self.name, max_size=260)
+        pdf.set_xy(12 + xplus, 73 + yplus)
         pdf.cell(230, 24, txt=self.name, ln=1, align="C")
         
         # Media
-        pdf.set_font(normal_font_name, size=12)
-        pdf.set_xy(10 + xplus, 87 + yplus)
+        set_fitted_font_size(self.name, max_size=260)
+        pdf.set_xy(12 + xplus, 94 + yplus)
         pdf.cell(230, 24, txt=self.media, ln=1, align="C")
 
         # Type: Print/Original
         # Display "Original", "Print", "Print X (Open Edition)", or "Print X of Y"
         
         if self.type == c.ORIGINAL:
-            pdf.set_xy(235 + xplus, 39 + yplus)
+            pdf.set_xy(232 + xplus, 50 + yplus)
             pdf.set_font(normal_font_name, size=12)
             pdf.cell(53, 24, txt="Original", ln=1, align="L")
         else: # PRINT
@@ -127,31 +127,31 @@ class ArtShowPiece:
             if self.print_run_num and self.print_run_total:
                 pdf.set_font(normal_font_name, size=9)
 
-                pdf.set_xy(235 + xplus, 33 + yplus)
+                pdf.set_xy(233 + xplus, 45 + yplus)
                 pdf.cell(53, 24, txt=f"Print", ln=1, align="L")
 
-                pdf.set_xy(235 + xplus, 42 + yplus)
+                pdf.set_xy(233 + xplus, 54 + yplus)
                 pdf.cell(53, 24, txt=f"({self.print_run_num} of {self.print_run_total})", ln=1, align="L")
             # Only X entered, display "Print X (Open Edition)"
             elif self.print_run_num:
                 pdf.set_font(normal_font_name, size=9)
 
-                pdf.set_xy(235 + xplus, 33 + yplus)
+                pdf.set_xy(233 + xplus, 45 + yplus)
                 pdf.cell(53, 24, txt=f"Print {self.print_run_num}", ln=1, align="L")
 
-                pdf.set_xy(235 + xplus, 42 + yplus)
+                pdf.set_xy(233 + xplus, 54 + yplus)
                 pdf.cell(53, 24, txt=f"Open Edition", ln=1, align="L")
             # No X of Y entered, display "Print"
             else:
-                pdf.set_xy(235 + xplus, 39 + yplus)
+                pdf.set_xy(241 + xplus, 50 + yplus)
                 pdf.set_font(normal_font_name, size=12)
                 pdf.cell(53, 24, txt="Print", ln=1, align="L")
 
         # Minimum Bid, QuickSale Price
-        pdf.set_font(bold_font_name, size=12)
-        pdf.set_xy(228 + xplus, 70 + yplus)
+        pdf.set_font(bold_font_name, size=11)
+        pdf.set_xy(219 + xplus, 78 + yplus)
         pdf.cell(53, 14, txt=str(self.opening_bid) if self.valid_for_sale else 'NFS', ln=1, align="R")
-        pdf.set_xy(228 + xplus, 92 + yplus)
+        pdf.set_xy(219 + xplus, 99 + yplus)
         pdf.cell(53, 14, txt=str(self.quick_sale_price) if self.valid_quick_sale else 'NFS', ln=1, align="R")
 
     @property
